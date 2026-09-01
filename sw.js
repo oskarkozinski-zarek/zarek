02dfdb07a7418e17cb52954e000c52b6d25c88ff
oskarkozinski-zarek <k-ski77@wp.pl>
2026-09-01 16:43:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,5 @@
-const CACHE='zarek-v5-20260901-firebase';
+const CACHE='zarek-v9-20260901-tax';
 const ASSETS=['./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
-
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
-});
-
-self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
-});
-
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET') return;
-  if(event.request.mode==='navigate'){
-    event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));
-    return;
-  }
-  event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request)));
-});
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;if(event.request.mode==='navigate'){event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));return;}event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request)));});
